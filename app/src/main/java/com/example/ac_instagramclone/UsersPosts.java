d72ccbf2beabaeae9a10093888f2a3b60088e359
paulg92 <paul.gules@gmail.com>
2019-10-16 13:07:28 +0200

    Added users info and some other improvements

--- app/src/main/java/com/example/ac_instagramclone/UsersPosts.java
+++ app/src/main/java/com/example/ac_instagramclone/UsersPosts.java
@@ -2,13 +2,103 @@ package com.example.ac_instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.List;
 
 public class UsersPosts extends AppCompatActivity {
+    private LinearLayout mLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users_posts);
+
+        mLinearLayout = findViewById(R.id.linearLayout);
+
+        Intent receivedIntentObject = getIntent();
+        final String receivedUsersName = receivedIntentObject.getStringExtra("username");
+
+        FancyToast.makeText(this, receivedUsersName, Toast.LENGTH_SHORT, FancyToast.SUCCESS, true).show();
+
+        setTitle(receivedUsersName + "'s posts");
+
+        ParseQuery<ParseObject> parseQuery = new ParseQuery<>("Photo");
+        parseQuery.whereEqualTo("username", receivedUsersName);
+        parseQuery.orderByDescending("createdAt");
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+
+        parseQuery.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if (objects.size() > 0 && e == null) {
+                    for (ParseObject post : objects) {
+                        final TextView postDescription = new TextView(UsersPosts.this);
+                        postDescription.setText(post.get("image_des") + "");
+                        ParseFile postPicture = (ParseFile) post.get("picture");
+                        postPicture.getDataInBackground(new GetDataCallback() {
+                            @Override
+                            public void done(byte[] data, ParseException e) {
+                                if (data != null && e == null) {
+                                    Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                                    ImageView postImageView = new ImageView(UsersPosts.this);
+                                    LinearLayout.LayoutParams image_params = new LinearLayout.LayoutParams
+                                            (ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                                    image_params.setMargins(5, 5, 5, 5);
+                                    postImageView.setLayoutParams(image_params);
+                                    postImageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                                    postImageView.setImageBitmap(bitmap);
+
+                                    LinearLayout.LayoutParams desc_params = new LinearLayout.LayoutParams
+                                            (ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                                    desc_params.setMargins(5, 5, 5, 5);
+                                    postDescription.setLayoutParams(desc_params);
+                                    postDescription.setGravity(Gravity.CENTER);
+                                    postDescription.setBackgroundColor(Color.BLUE);
+                                    postDescription.setTextColor(Color.WHITE);
+                                    postDescription.setTextSize(30f);
+
+                                    mLinearLayout.addView(postImageView);
+                                    mLinearLayout.addView(postDescription);
+                                }
+                            }
+                        });
+                    }
+                } else {
+                    FancyToast.makeText(UsersPosts.this, receivedUsersName +
+                                    "doesn't have any posts", FancyToast.LENGTH_SHORT, FancyToast.INFO,
+                            true).show();
+                    finish();
+                }
+
+                progressDialog.dismiss();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/ac_instagramclone/UsersTab.java
+++ app/src/main/java/com/example/ac_instagramclone/UsersTab.java
@@ -1,6 +1,7 @@
 package com.example.ac_instagramclone;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,26 +9,33 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import libs.mjn.prettydialog.PrettyDialog;
+import libs.mjn.prettydialog.PrettyDialogCallback;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class UsersTab extends Fragment {
+public class UsersTab extends Fragment implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
 
     private ListView mListView;
-    private ArrayList mArrayList;
+    private ArrayList<String> mArrayList;
     private ArrayAdapter mArrayAdapter;
 
     public UsersTab() {
@@ -43,6 +51,9 @@ public class UsersTab extends Fragment {
         mListView = view.findViewById(R.id.listView);
         mArrayList = new ArrayList();
         mArrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, mArrayList);
+
+        mListView.setOnItemClickListener(UsersTab.this);
+        mListView.setOnItemLongClickListener(UsersTab.this);
 
         final TextView txtLoadingData = view.findViewById(R.id.loadingUsersDialog);
 
@@ -64,9 +75,42 @@ public class UsersTab extends Fragment {
                 }
             }
         });
-
         return view;
-
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent = new Intent(getContext(), UsersPosts.class);
+        intent.putExtra("username", mArrayList.get(position));
+        startActivity(intent);
+    }
+
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+        parseQuery.whereEqualTo("username", mArrayList.get(position));
+        parseQuery.getFirstInBackground(new GetCallback<ParseUser>() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (user != null && e == null) {
+
+                    /*FancyToast.makeText(getContext(), user.get("profileProfession") + "",
+                            Toast.LENGTH_SHORT, FancyToast.SUCCESS, true).show();*/
+                    final PrettyDialog prettyDialog = new PrettyDialog(getContext());
+
+                    prettyDialog.setTitle(user.getUsername() + " 's Info").setMessage(user.get("bio") + "\n"
+                    + user.get("profileprofession") + "\n" + user.get("profileHobbies") + "\n" +
+                            user.get("profileFavSport")).setIcon(R.drawable.person).addButton("Ok",
+                            R.color.pdlg_color_white, R.color.pdlg_color_green, new PrettyDialogCallback() {
+                                @Override
+                                public void onClick() {
+                                    prettyDialog.dismiss();
+                                }
+                            }).show();
+                }
+            }
+        });
+        return true;
+    }
 }
